f6b7a0fc6856ac38a46a297724adce3e08f356ac
Arman170616 <armanice09@gmail.com>
2023-01-28 16:29:30 +0600

    app opening location set with previous location

--- src/main.js
+++ src/main.js
@@ -1,9 +1,29 @@
 const { BrowserWindow, app } = require('electron');
+const windowStateKeeper = require('electron-window-state');
 
 function createWindow() {
 
-    const mainWindow = new BrowserWindow();
-    mainWindow.loadFile('index.html');
+      // Load the previous state with fallback to defaults
+  let mainWindowState = windowStateKeeper({
+    defaultWidth: 1000,
+    defaultHeight: 800
+  });
+ 
+  // Create the window using the state information
+  const mainWindow = new BrowserWindow({
+    x: mainWindowState.x,
+    y: mainWindowState.y,
+    width: mainWindowState.width,
+    height: mainWindowState.height,
+    title: "This is the main window"
+  });
+
+  // const mainWindow = new BrowserWindow();
+mainWindow.loadFile('src/index.html');
+
+    // automatically (the listeners will be removed when the window is closed)
+  // and restore the maximized or full screen state
+  mainWindowState.manage(mainWindow);
 }
 
 
